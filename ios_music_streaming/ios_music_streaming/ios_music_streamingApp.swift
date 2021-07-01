4ef569645887c0eaaff99c5b2302ff6573f5bb49
Robin Kim <robinkim@Robins-MBP.fios-router.home>
2021-07-01 16:32:58 -0400

    swiftui setup

---
+++ ios_music_streaming/ios_music_streaming/ios_music_streamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_music_streamingApp.swift
+//  ios_music_streaming
+//
+//  Created by Robin Kim on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_music_streamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
