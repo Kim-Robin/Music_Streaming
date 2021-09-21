c99935f318a5f57c432fe535cc5c76d0633a8f0c
Robin Kim <41379739+Kim-Robin@users.noreply.github.com>
2021-09-21 14:30:56 -0400

    added eslint for dev

--- web_music_streaming/src/App.tsx
+++ web_music_streaming/src/App.tsx
@@ -4,6 +4,8 @@ import LOGO from './logo.svg';
 import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
+    const num = 'Vishwas';
+
     return <>
         <h1>yo this React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
         <img src={IMAGE} alt="React Logo" width="300" height="300"/>
