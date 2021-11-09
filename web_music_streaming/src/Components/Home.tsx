f14374629e68d5142176561501091538a4a047fc
Robin Kim <41379739+Kim-Robin@users.noreply.github.com>
2021-11-09 14:48:55 -0500

    minor css change

--- web_music_streaming/src/Components/Home.tsx
+++ web_music_streaming/src/Components/Home.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="main-box">
-            <h1 className="main-box-contents">Home page</h1>
+            <div className="main-box-contents-heading">Home page</div>
+            <div className="main-box-contents"></div>
         </div>
     )
 }
